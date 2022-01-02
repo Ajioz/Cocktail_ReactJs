cc4adadb499fa2e9eff20d4a2bdb48d7571e6060
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2022-01-02 23:29:00 +0100

    uploading the build

---
+++ build/precache-manifest.dc5e731e2068982f29b1d55cf2824c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893052ccd379469e18abe2479f194a54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77ebc17ad4de87359ebb",
+    "url": "/static/css/main.db6e84ad.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ebc17ad4de87359ebb",
+    "url": "/static/js/main.bcfa1c8d.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/static/media/logo.f3ad7664.svg"
+  }
+]);
