f899070edbbef1685ff8991980cdc00862c00a6c
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2022-01-02 23:16:57 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos at sapiente quis possimus eos recusandae quibusdam cumque error tempora modi sequi ad eligendi, reprehenderit eaque fugiat accusamus laudantium aspernatur nihil vero tempore, officiis voluptate. Mollitia optio dolorum eaque atque iusto!
+      </p>
+    </section>
+  )
+}
+
+export default About
